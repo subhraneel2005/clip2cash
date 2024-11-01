b9ee8c9b00eeb92576d9eed23cd34166564e4cb4
Subhamay-Dey <ncdey1966@gmail.com>
2024-11-01 17:25:30 +0530

    Next Auth authentication done ✅.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Dashboard from "@/components/Dashboard";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <Button>GetStart</Button>
-    </>
+    <main className="max-w-7xl mx-auto my-12 space-y-5">
+      <Dashboard/>
+    </main>
   );
 }
