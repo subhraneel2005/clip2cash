37fa887db7fb92a5a90e15deeba2324c1f4fb546
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-11-04 21:04:25 +0530

    login page made

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,8 @@
-import { LogIn } from '@/components/auth-components/Login'
+import Login from '@/components/auth-components/Login'
 import React from 'react'
 
 export default function page() {
   return (
-    <LogIn/>
+    <Login/>
   )
 }
--- src/components/auth-components/Login.tsx
+++ src/components/auth-components/Login.tsx
@@ -1,19 +1,82 @@
 'use client'
 
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { signIn } from "next-auth/react"
-import { FaGithub, FaGoogle } from "react-icons/fa"
+import { GitHubLogoIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
+import React from 'react'
+import { Button } from '../ui/button';
+import { signIn } from 'next-auth/react';
 
-export function LogIn() {
+export default function Login() {
   return (
-    <div className="min-h-screen w-full flex justify-center items-center">
-    <Button onClick={(e) => {
-      e.preventDefault();
-      signIn('github', {callbackUrl:"/"})}}>Github</Button>
-    <Button onClick={(e) => {
-      e.preventDefault();
-      signIn('google', {callbackUrl:"/"})}}>Google</Button>
+    <div className="grid min-h-screen grid-cols-1 md:grid-cols-2">
+      <div className="relative hidden flex-col justify-between p-10 dark:border-r md:flex">
+        <div className="flex items-center space-x-2">
+          <div className="h-6 w-6 text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z" />
+            </svg>
+          </div>
+          <span className="text-xl font-bold">Acme Inc</span>
+        </div>
+        <div className="space-y-2">
+          <p className="text-lg font-medium leading-none">
+            "This library has saved me countless hours of work and helped me deliver stunning designs to my clients faster than ever before."
+          </p>
+          <p className="text-sm text-gray-300">Sofia Davis</p>
+        </div>
+      </div>
+      <div className="flex items-center justify-center px-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">Create an account</h1>
+            <p className="text-sm text-muted-foreground">Enter your email below to create your account</p>
+          </div>
+          <div className="grid gap-4">
+            <Button 
+            onClick={() => {
+              signIn('google', {callbackUrl:"/"})}}
+            variant="outline">
+              <svg viewBox="0 0 24 24" className="mr-2 h-4 w-4">
+                <path
+                  d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                  fill="#4285F4"
+                />
+                <path
+                  d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                  fill="#34A853"
+                />
+                <path
+                  d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+                  fill="#FBBC05"
+                />
+                <path
+                  d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+                  fill="#EA4335"
+                />
+              </svg>
+              Google
+            </Button>
+            <Button
+              onClick={() => {
+                signIn('github', {callbackUrl:"/"})}}
+             variant="outline">
+              <GitHubLogoIcon className="mr-2 h-4 w-4" />
+              GitHub
+            </Button>
+          </div>
+          <div className="px-8 text-center text-sm text-muted-foreground">
+            By clicking continue, you agree to our{" "}
+            <Link href="/terms" className="underline underline-offset-4 hover:text-primary">
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link href="/privacy" className="underline underline-offset-4 hover:text-primary">
+              Privacy Policy
+            </Link>
+            .
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
