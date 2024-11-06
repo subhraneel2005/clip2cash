7b9df22faf2922863a8f418ce1498ee547ad7ec1
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-11-06 14:23:05 +0530

    bug removed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,17 +3,6 @@ import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import prisma from '@/lib/prisma';
-import nodemailer from 'nodemailer';
-
-export const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
-  },
-});
 
 export const authOptions: AuthOptions = {
   pages: {
@@ -31,30 +20,6 @@ export const authOptions: AuthOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET!,
-  events: {
-    async signIn({ user }) {
-      // Log the user object to check if email is present
-      console.log("SignIn event triggered. User object:", user);
-
-      if (user?.email) {
-        const mailOptions = {
-          from: process.env.FROM_EMAIL,
-          to: user.email,
-          subject: 'Welcome to GetStart!',
-          text: `Hello ${user.name || 'User'}, welcome to GetStart! We're glad to have you 😄.`
-        };
-
-        try {
-          await transporter.sendMail(mailOptions);
-          console.log("Welcome email sent successfully.");
-        } catch (error) {
-          console.error("Error sending welcome email:", error);
-        }
-      } else {
-        console.error("User email is missing or null. Cannot send welcome email.");
-      }
-    },
-  },
 };
 
 const handler = NextAuth(authOptions);
