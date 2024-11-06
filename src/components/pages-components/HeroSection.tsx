a221b42c1733537bd43dfecdeff06eb35794b638
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-11-07 00:22:01 +0530

    added simple texts

--- src/components/pages-components/HeroSection.tsx
+++ src/components/pages-components/HeroSection.tsx
@@ -12,13 +12,15 @@ export default function HeroSection() {
         className='flex flex-col items-center justify-center text-center pt-10'>
           {/* Add your hero text here 👇 */}
           <h1 className="lg:max-w-6xl text-4xl py-3 px-6 lg:text-6xl bg-gradient-to-b from-gray-900 via-black to-gray-600 bg-clip-text tracking-tighter text-transparent font-black text-center dark:from-white dark:via-gray-200 dark:to-gray-400 lg:mt-[50px]">
-            Launch your product in a snap
+            Your awesome hero text here
+
           </h1>
 
           {/* Add your hero description here 👇 */}
           <p className='text-[14px] md:text-[15px] text-zinc-400 px-5 max-w-[500px] mb-5 pt-2'>
-            The best fullstack kit with pre-built integrations. 
-            Focus on your core idea. Increase the creativity.
+            The hero description goes here. This is a great place to tell your story and showcase your brand.
+
+
           </p>
 
           {/* Add your CTA button here 👇 */}
