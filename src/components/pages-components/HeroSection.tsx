e04f820b02693c198e71b82a30758de1b49d27b9
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-11-07 13:56:44 +0530

    added simple animation and fuzed a bug

--- src/components/pages-components/HeroSection.tsx
+++ src/components/pages-components/HeroSection.tsx
@@ -1,4 +1,4 @@
-
+'use client'
 import React from 'react'
 import { Button } from '../ui/button';
 import { ArrowRightIcon } from 'lucide-react';
@@ -7,25 +7,24 @@ import {motion} from "framer-motion"
 export default function HeroSection() {
   
   return (
-    <div className=' min-h-screen w-full flex flex-col items-center justify-center bg-[url("/gb.png")] bg-center bg-cover'>
+    <div className='min-h-screen w-full flex flex-col items-center justify-center bg-[url("/gb.png")] bg-center bg-cover'>
       {/* Hero Texts 📝 */}
       <div 
         className='flex flex-col items-center justify-center text-center pt-10'>
           {/* Add your hero text here 👇 */}
-          <motion.div
+          <motion.h1
               initial={{ opacity: 0, y: 20 }}
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5 }}
+              className="lg:max-w-6xl text-4xl py-3 px-6 lg:text-6xl bg-gradient-to-b from-gray-900 via-black to-gray-600 bg-clip-text tracking-tighter text-transparent font-black text-center dark:from-white dark:via-gray-200 dark:to-gray-400 lg:mt-[50px]"
             >
-          <h1 className="lg:max-w-6xl text-4xl py-3 px-6 lg:text-6xl bg-gradient-to-b from-gray-900 via-black to-gray-600 bg-clip-text tracking-tighter text-transparent font-black text-center dark:from-white dark:via-gray-200 dark:to-gray-400 lg:mt-[50px]">
             Your awesome hero text here
-          </h1>
+          </motion.h1>
 
           {/* Add your hero description here 👇 */}
           <p className='text-[14px] md:text-[15px] text-zinc-400 px-5 max-w-[500px] mb-5 pt-2'>
             The hero description goes here. This is a great place to tell your story and showcase your brand. Your awesome description.
           </p>
-          </motion.div>
           {/* Add your CTA button here 👇 */}
           <Button className='mb-12'>
             <p>Get Started</p>
